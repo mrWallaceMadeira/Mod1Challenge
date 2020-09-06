b0c40e0e5515a08f9ebe77e33b332e876116be0f
Mr. Wallace <pwallace@madeira.org>
2020-09-06 16:06:47 -0400

    adding week2 challenge

---
+++ src/madeira/cs/Week2.java
@@ -0,0 +1,62 @@
+package madeira.cs;
+
+public class Week2 {
+    public static void main(String[] args) {
+        /*
+            Problem I
+            --------
+            Write a program that prints your full name with the following stipulations
+                a. It is printed on one line
+                b. You use 1 print statement for each part of your name (i.e. first, middle, last)
+         */
+        
+        // Problem I below
+
+
+
+
+
+        /*
+            Problem II
+            --------
+         */
+        /*
+            Problem III
+            --------
+         */
+        /*
+            Problem IV
+            --------
+         */
+        /*
+            Problem V
+            --------
+         */
+
+        // ==================
+        // EXTRA CREDIT BELOW
+        // ==================
+
+
+        /*
+            Problem VI
+            --------
+         */
+        /*
+            Problem VII
+            --------
+         */
+        /*
+            Problem VIII
+            --------
+         */
+        /*
+            Problem IX
+            --------
+         */
+        /*
+            Problem X
+            --------
+         */
+    }
+}
---
+++ src/madeira/cs/Week3.java
@@ -0,0 +1,7 @@
+package madeira.cs;
+
+public class Week3 {
+    public static void main(String[] args) {
+
+    }
+}
