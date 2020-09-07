1cbb228efc87836ab5859905a392c02666d2df5c
Mr. Wallace <pwallace@madeira.org>
2020-09-07 06:54:44 -0400

    updating

--- src/madeira/cs/Week2.java
+++ src/madeira/cs/Week2.java
@@ -5,12 +5,11 @@ public class Week2 {
         /*
             Problem I
             --------
-            Write a program that prints your full name with the following stipulations
-                a. It is printed on one line
-                b. You use 1 print statement for each part of your name (i.e. first, middle, last)
+            Write a program that prints your full name
+            You must:
+                a. Print your full name on a single output line
+                b. Use 1 print statement for each part of your name (i.e. first, middle, last)
          */
-        
-        // Problem I below
 
 
 
@@ -19,19 +18,47 @@ public class Week2 {
         /*
             Problem II
             --------
+            Write a program that prints "Hello World"
+            You must:
+                a. Print on two separate lines
+                b. Not use the "println()" method
          */
+
+
+
+
+
+
         /*
             Problem III
             --------
+            Write a program that reads your age in years
+            You must:
+                a. Print the variable type of their age (i.e. int)
          */
+
+
+
+
+
+
+
+
+
         /*
             Problem IV
             --------
+            Write a program that
+
          */
         /*
             Problem V
             --------
          */
+
+
+
+
 
         // ==================
         // EXTRA CREDIT BELOW
