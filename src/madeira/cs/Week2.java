a9967c6f20f12243f6b2d51fa7e1ee5c1de19960
Mr. Wallace <pwallace@madeira.org>
2020-09-07 07:23:13 -0400

    problems 1-10 [x]

--- src/madeira/cs/Week2.java
+++ src/madeira/cs/Week2.java
@@ -1,5 +1,7 @@
 package madeira.cs;
 
+import java.util.Scanner;
+
 public class Week2 {
     public static void main(String[] args) {
         /*
@@ -44,19 +46,25 @@ public class Week2 {
 
 
 
-
         /*
             Problem IV
             --------
-            Write a program that
-
+            Write a program that reads your name and prints each letter on a new line of output
          */
+
+
+
+
+
+
         /*
             Problem V
             --------
+            Fix the errors such that the following will print your name
+
          */
-
-
+            Scanner myScanner = new Scanner(System.in);
+            System.out.print(myScanner.nextInt());
 
 
 
@@ -68,22 +76,89 @@ public class Week2 {
         /*
             Problem VI
             --------
+            Write a program that prints your age in 5 years
+            You must:
+                a. Read your current age
+                b. Manipulate that number
          */
+
+
+
+
+
+
+
+
+
         /*
             Problem VII
             --------
+            Write a program that reads 3 numbers, sums them & divides the sum by 2
+            You must:
+                a. Print the result
+                b. Use a mix of decimals and integers
+                c. Your result should be a decimal
          */
+
+
+
+
+
+
+
         /*
             Problem VIII
             --------
+            Write a program that reads in a string and identifies if the string is your name
+            You must:
+                a. Give the user 3 guesses
+                b. Present them with a success or fail message
          */
+
+
+
+
+
+
         /*
             Problem IX
             --------
+            Write a program that reads input from a file called "readme.txt"
+            You must:
+                a. Print the content of the file
+            Hints:
+                a. Create the file, write some stuff, save it to the same folder as your program
          */
+
+
+
+
+
+
+
+
+
         /*
             Problem X
             --------
+            Write a program that reads a 10 digit number & adds 1 to each DIGIT in the number.
+            Print the result as a recombined whole number
+            e.g.
+            Initial number: 12345678910
+            New number: 234567891011
+
          */
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
