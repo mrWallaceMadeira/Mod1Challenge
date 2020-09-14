d9cb669075465b4e07f906fc2d2f324966642f9d
Mr. Wallace <67603153+mrWallaceMadeira@users.noreply.github.com>
2020-09-14 08:45:13 -0400

    Update Week3.java
    Adding exercises

--- src/madeira/cs/Week3.java
+++ src/madeira/cs/Week3.java
@@ -2,6 +2,49 @@ package madeira.cs;
 
 public class Week3 {
     public static void main(String[] args) {
-
+        /*
+            Problem I
+            ---------
+            Prompt the user to enter two numbers
+            Compare the two numbers and print out "First" if first is greater or "Second" second is greater
+            This should handle decimals and integers at a wide range of values
+            
+            
+            Problem II
+            ----------
+            Prompt the user to enter their favorite genre of music
+            Reply with "Same!" if it is the same as your favorite genre or "Lame." if not.
+            
+            
+            
+            Problem III
+            -----------
+            Declare an integer, any integer.
+            Prompt the user to guess your number.
+            Give them 10 tries to get it right.
+            If they do, present them with a congratulatory message, if not, roast them unfairly.
+            
+            
+            
+            Problem IV
+            ----------
+            
+            
+            Problem V:
+            
+            
+            
+            ------------------
+            EXTRA CREDIT BELOW
+            ------------------
+            
+            
+            
+            Problem VI:
+            Problem VII:
+            Problem VIII:
+            Problem IX:
+            Problem X:
+        */
     }
 }
